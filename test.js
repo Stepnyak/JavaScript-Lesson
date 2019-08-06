51b688dc03a8b43c0f38518185bd6c6caddb775c
Владимир <kursinov@mail.ru>
2019-08-06 10:11:34 +0300

    Мой первый коммит

---
+++ test.js
@@ -0,0 +1,12 @@
+let vopros1=prompt("Ваш бюджет ?");
+//confirm(vopros1);
+alert(vopros1 + "  О этого достаточно ");
+let vopros2 = prompt("Название вашего магазина?  ");
+alert(vopros2 + "  Отлично я знаю этот магазин ");
+let open=function(){console.log('Открыто')};
+Staff={"Менеджер":'Курсинов',"Бухгалтер":'Манджиева ЛГ',"Продавец":'Иванова'};
+shopGoods=['Стол','Табурет','Молоток','Шуруповерт'];
+mainList = { "Бюджет": 5000,"Имя магазина":'Лакомка',Staff,shopGoods,open};
+console.log(mainList);
+console.log(mainList.shopGoods);
+console.log(mainList.open());
